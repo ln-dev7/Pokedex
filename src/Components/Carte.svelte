a96a05d81d3385f428140d179c099f78465a6e3f
ln-dev7 <leonelngoya@gmail.com>
2022-02-14 14:04:21 +0100

    visibiliter des cartes

---
+++ src/Components/Carte.svelte
@@ -0,0 +1,32 @@
+<script>
+    export let name;
+    export let pic;
+</script>
+
+<div class="carte">
+    <p class="nom">{name}</p>
+    <img src={pic} alt="pokemon">
+</div>
+
+<style>
+    .carte{
+        width: 300px;
+        height: 200px;
+        border: 1px solid #ffcc01;
+        background: rgba(44, 108, 181, 0.1);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        margin: 5px;
+        cursor: pointer;
+    }
+
+    .carte img{
+        width: 100px;
+    }
+
+    .carte p{
+        font-size: 25px;
+    }
+</style>
--- src/Components/Contenu.svelte
+++ src/Components/Contenu.svelte
@@ -1,21 +1,62 @@
 <script>
-	import BarreRecherche from "./BarreRecherche.svelte";
+  import BarreRecherche from "./BarreRecherche.svelte";
+  import Carte from "./Carte.svelte";
+  import { v4 as uuidv4 } from "uuid";
+
+  let allPokemon = [];
+  let tableauFin = [];
+
+  function fetchPokemonBase() {
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=301")
+      .then((reponse) => reponse.json())
+      .then((allPoke) => {
+        allPoke.results.forEach((pokemon) => {
+          fetchPokemonComplet(pokemon);
+        });
+      });
+  }
+  fetchPokemonBase();
+
+  function fetchPokemonComplet(pokemon) {
+    let objPokemonFull = {};
+    let url = pokemon.url;
+    let nameP = pokemon.name;
+
+    fetch(url)
+      .then((reponse) => reponse.json())
+      .then(function (pokeData) {
+        objPokemonFull.pic = pokeData.sprites.back_default;
+      });
+
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${nameP}/`)
+      .then((reponse) => reponse.json())
+      .then(function (pokeData) {
+        objPokemonFull.name = pokeData.names[4].name;
+        allPokemon.push(objPokemonFull);
+      })
+      .then(() => {
+        tableauFin = allPokemon.slice(0, 30);
+      });
+  }
 </script>
 
+<BarreRecherche />
 <div class="contenu">
-    <BarreRecherche />
+  {#each tableauFin as pokemon (uuidv4())}
+    <Carte name={pokemon.name} pic={pokemon.pic} />
+  {/each}
 </div>
 
 <style>
-    /* #2c6cb5
+  /* #2c6cb5
     #ffcc01 */
-    .contenu{
-        max-width: 1400px;
-        width: 95%;
-        padding: 0 50px;
-        margin: 0 auto;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-    }
+  .contenu {
+    max-width: 1400px;
+    width: 95%;
+    padding: 0 50px;
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
 </style>
